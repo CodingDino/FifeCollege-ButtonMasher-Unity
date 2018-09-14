d71e9f7feb7b2980dbe81b2f4b442b837a2724f3
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-14 13:02:57 +0100

    Added assets and starter script

---
+++ Assets/Scripts/ButtonMasher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMasher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Start method called");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
