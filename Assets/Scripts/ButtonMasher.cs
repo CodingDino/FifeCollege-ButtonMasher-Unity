b6ad57da3d9875a30209567a7efbf62db1febc30
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-14 15:27:37 +0100

    Added time up / game over

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -12,6 +12,7 @@ public class ButtonMasher : MonoBehaviour {
 
     private int score = 0; // the numerical data value of our score
     private float timeRemaining = 0; // Numerical time remaining for the game
+    private bool gameRunning = true;
 
 	// Use this for initialization
 	void Start () {
@@ -30,20 +31,36 @@ public class ButtonMasher : MonoBehaviour {
 
         // Update the visual time remaining
         timerText.text = (Mathf.CeilToInt(timeRemaining)).ToString();
+
+        // Check if we have run out of time
+        if (timeRemaining <= 0)
+        {
+            // Stop the game running
+            gameRunning = false;
+
+            // Stop our time from going negative
+            timeRemaining = 0;
+        } // End of if (timeRunning <= 0)
+
     } // End of Update()
 
     // Responds to event from Unity that the object has been clicked
     void OnMouseDown()
     {
         Debug.Log("OnMouseDown method called");
-        // Trigger our clicking sound to play
-        clickSound.Play();
-        // Increase the score by 1
-        score = score + 1;
-        // Update visual score
-        scoreText.text = score.ToString();
+
+        // Check if the game is still running....
+        if (gameRunning == true)
+        {
+            // Trigger our clicking sound to play
+            clickSound.Play();
+            // Increase the score by 1
+            score = score + 1;
+            // Update visual score
+            scoreText.text = score.ToString();
+        } // End of if (gameRunning == true)
+
     } // End of OnMouseDown()
-
-
+    
 } // End of ButtonMasher class
 
