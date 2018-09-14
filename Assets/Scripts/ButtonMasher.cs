b5803252206f43ef7593da486022b44a85350e17
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-09-14 15:10:36 +0100

    Time remaining added

--- Assets/Scripts/ButtonMasher.cs
+++ Assets/Scripts/ButtonMasher.cs
@@ -7,18 +7,29 @@ public class ButtonMasher : MonoBehaviour {
     // Data for our class
     public AudioSource clickSound; // sound that will play when we click the button
     public TextMesh scoreText; // display text for the player's score
+    public TextMesh timerText; // display text for the timer remaining
+    public float gameLength; // how many seconds will the game last
 
     private int score = 0; // the numerical data value of our score
+    private float timeRemaining = 0; // Numerical time remaining for the game
 
 	// Use this for initialization
 	void Start () {
         Debug.Log("Start method called");
+
+        timeRemaining = gameLength;
 	} // End of Start()
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log("Update method called");
+
+        // Update the numerical time remaining
+        timeRemaining = timeRemaining - Time.deltaTime;
+
+        // Update the visual time remaining
+        timerText.text = (Mathf.CeilToInt(timeRemaining)).ToString();
     } // End of Update()
 
     // Responds to event from Unity that the object has been clicked
